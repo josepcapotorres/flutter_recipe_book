6f77560bd7f22faeffa5aebb38e622e0d8c48c97
j2272 <josepcapotorres@gmail.com>
2022-07-03 19:09:09 +0200

    Apply the BasePage widget

--- lib/views/pages/random_food_page.dart
+++ lib/views/pages/random_food_page.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_myrecipesapp/controllers/recipe_controller.dart';
+import 'package:flutter_myrecipesapp/views/widgets/base_page.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:get/get.dart';
 
@@ -26,77 +25,53 @@ class _RandomFoodPageState extends State<RandomFoodPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BasePage(
       appBar: AppBar(
         title: Text(translate("random_food_page.title")),
       ),
       body: GetBuilder<RecipeController>(
         builder: (_) {
-          return _recipeController.loading ||
-                  _recipeController.randomRecipe == null
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : _recipeController.randomRecipe == null &&
-                      _recipeController.recipeList.isEmpty
-                  ? Center(
-                      child: Text(translate("common.no_results")),
-                    )
-                  : _recipeController.recipeList.isNotEmpty
-                      ? Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(_recipeController.randomRecipe!.name!),
-                              ElevatedButton(
-                                child: Text(
-                                  translate("random_food_page.recipe_details")
-                                      .toUpperCase(),
-                                ),
-                                onPressed: () {
-                                  Get.toNamed(
-                                    RecipeDetailPage.routeName,
-                                    arguments: _recipeController.randomRecipe,
-                                  );
-                                },
-                              ),
-                              ElevatedButton(
-                                child: Text(
-                                  translate("random_food_page.generate_recipe")
-                                      .toUpperCase(),
-                                ),
-                                onPressed: () {
-                                  _recipeController.generateRandomRecipe();
-                                },
-                              ),
-                            ],
-                          ),
-                        )
-                      : Center(
-                          child: Text(translate("common.no_results")),
-                        );
+          if (_recipeController.loading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (_recipeController.recipeList.isNotEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(_recipeController.randomRecipe!.name!),
+                  ElevatedButton(
+                    child: Text(
+                      translate("random_food_page.recipe_details")
+                          .toUpperCase(),
+                    ),
+                    onPressed: () {
+                      Get.toNamed(
+                        RecipeDetailPage.routeName,
+                        arguments: _recipeController.randomRecipe,
+                      );
+                    },
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      translate("random_food_page.generate_recipe")
+                          .toUpperCase(),
+                    ),
+                    onPressed: () {
+                      _recipeController.generateRandomRecipe();
+                    },
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: Text(translate("common.no_results")),
+            );
+          }
         },
       ),
-
-      /*Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _selectedValue != null ? Text(_selectedValue!) : Container(),
-              ElevatedButton(
-                child: Text("Generar recepta"),
-                onPressed: () {
-                  int randNum = _generateRandomNumber(0, _foodList.length);
-
-                  setState(() {
-                    _selectedValue = _foodList[randNum];
-                  });
-                },
-              ),
-            ],
-          ),
-        ),
-      ),*/
     );
   }
 }
